1110429d4311a740dd6e5d6a5acdb4cfa6ed251a
kuakling <kuakling@gmail.com>
2015-07-28 11:36:45 +0700

    Create ActionColumn.php

---
+++ grid/ActionColumn.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace kuakling\materialize\grid;
+
+use Yii;
+use Closure;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn as BootstrapActionColumn;
+
+/**
+ * ActionColumn is a column for the [[GridView]] widget that displays buttons for viewing and manipulating the items.
+ *
+ * To add an ActionColumn to the gridview, add it to the [[GridView::columns|columns]] configuration as follows:
+ *
+ * ```php
+ * 'columns' => [
+ *     // ...
+ *     [
+ *         'class' => ActionColumn::className(),
+ *         // you may configure additional properties here
+ *     ],
+ * ]
+ * ```
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class ActionColumn extends BootstrapActionColumn
+{
+    /**
+     * Initializes the default button rendering callbacks.
+     */
+    protected function initDefaultButtons()
+    {
+        if (!isset($this->buttons['view'])) {
+            $this->buttons['view'] = function ($url, $model, $key) {
+                $options = array_merge([
+                    'title' => Yii::t('yii', 'View'),
+                    'aria-label' => Yii::t('yii', 'View'),
+                    'data-pjax' => '0',
+                ], $this->buttonOptions);
+                return Html::a('<i class="material-icons">visibility</i>', $url, $options);
+            };
+        }
+        if (!isset($this->buttons['update'])) {
+            $this->buttons['update'] = function ($url, $model, $key) {
+                $options = array_merge([
+                    'title' => Yii::t('yii', 'Update'),
+                    'aria-label' => Yii::t('yii', 'Update'),
+                    'data-pjax' => '0',
+                ], $this->buttonOptions);
+                return Html::a('<i class="material-icons">edit</i>', $url, $options);
+            };
+        }
+        if (!isset($this->buttons['delete'])) {
+            $this->buttons['delete'] = function ($url, $model, $key) {
+                $options = array_merge([
+                    'title' => Yii::t('yii', 'Delete'),
+                    'aria-label' => Yii::t('yii', 'Delete'),
+                    'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                    'data-method' => 'post',
+                    'data-pjax' => '0',
+                ], $this->buttonOptions);
+                return Html::a('<i class="material-icons">delete</i>', $url, $options);
+            };
+        }
+    }
+
+    
+}
